dd35aff22e9a65c4d98e098850d1afad54791912
mihaylinsergey <semi999@mail.ru>
2022-02-21 09:51:51 +0300

    2.1. Преобразование даты [#289476 #267511]

--- src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -23,29 +23,30 @@ public class SqlRuDateTimeParser implements DateTimeParser {
             entry("ноя", "11"),
             entry("дек", "12")
     );
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm");
+    private static final DateTimeFormatter TEMP_FORMATTER = DateTimeFormatter.ofPattern("yy-MM-dd");
+    private static final String TODAY = "сегодня";
+    private static final String YESTERDAY = "вчера";
 
     @Override
     public LocalDateTime parse(String parse) {
         LocalDateTime rsl = null;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm");
-        DateTimeFormatter formatterTemp = DateTimeFormatter.ofPattern("yy-MM-dd");
+        LocalDate date = LocalDate.now();
         String[] dataParse = parse.split(",");
-        if ("сегодня".contains(dataParse[0])) {
-            LocalDate date = LocalDate.now();
-            String text = date.format(formatterTemp);
-            rsl = LocalDateTime.parse(text + dataParse[1], formatter);
-        } else if ("вчера".contains(dataParse[0])) {
-            LocalDate date = LocalDate.now().minusDays(1);
-            String text = date.format(formatterTemp);
-            rsl = LocalDateTime.parse(text + dataParse[1], formatter);
+        if (TODAY.contains(dataParse[0])) {
+            String text = date.format(TEMP_FORMATTER);
+            rsl = LocalDateTime.parse(text + dataParse[1], FORMATTER);
+        } else if (YESTERDAY.contains(dataParse[0])) {
+            String text = date.minusDays(1).format(TEMP_FORMATTER);
+            rsl = LocalDateTime.parse(text + dataParse[1], FORMATTER);
         } else {
             String[] dataParseThen = dataParse[0].split(" ");
             int year = Integer.parseInt(dataParseThen[2]);
             int month = Integer.parseInt(MONTHS.get(dataParseThen[1]));
             int day  = Integer.parseInt(dataParseThen[0]);
-            LocalDate date = LocalDate.of(year, month, day);
-            String text = date.format(formatterTemp);
-            rsl = LocalDateTime.parse(text + dataParse[1], formatter);
+            date = LocalDate.of(year, month, day);
+            String text = date.format(TEMP_FORMATTER);
+            rsl = LocalDateTime.parse(text + dataParse[1], FORMATTER);
         }
         return rsl;
     }
