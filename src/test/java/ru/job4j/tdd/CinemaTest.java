1675cd9f5c17c7717fdd9fee1d2b0b712cb823d2
mihaylinsergey <semi999@mail.ru>
2022-03-21 23:20:50 +0300

    2. Что такое TDD? [#4918 #277954]

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -2,6 +2,8 @@ package ru.job4j.tdd;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
+
+import org.junit.Ignore;
 import org.junit.Test;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -9,6 +11,7 @@ import java.util.List;
 
 public class CinemaTest {
 
+    @Ignore
     @Test
     public void whenBuy() {
         Account account = new AccountCinema();
@@ -16,37 +19,45 @@ public class CinemaTest {
         Calendar date = Calendar.getInstance();
         date.set(2020, 10, 10, 23, 00);
         Ticket ticket = cinema.buy(account, 1, 1, date);
-        System.out.println(ticket);
-        Ticket exp = null;
-        assertThat(ticket, is(exp));
+        assertThat(ticket, is(new Ticket3D()));
     }
 
+    @Ignore
     @Test
     public void whenFind() {
         Cinema cinema = new Cinema3D();
         cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> true);
-        Session exp = null;
-        assertThat(sessions, is(exp));
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
     }
 
-    @Test
-    public void whenNoTickets() {
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenInvalidPlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, -1, 1000000, date);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenInvalidDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 42, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenThePlaceIsOccupied() {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
         Calendar date = Calendar.getInstance();
         date.set(2020, 10, 10, 23, 00);
         Ticket ticket = cinema.buy(account, 1, 1, date);
-        System.out.println(ticket);
-        Ticket exp = null;
-        assertThat(ticket, is(exp));
-    }
-
-    public void whenNoMovie() {
-        Cinema cinema = new Cinema3D();
-        cinema.add(new Session3D());
-        List<Session> sessions = cinema.find(session -> false);
-        Session exp = null;
-        assertThat(sessions, is(exp));
     }
 }
