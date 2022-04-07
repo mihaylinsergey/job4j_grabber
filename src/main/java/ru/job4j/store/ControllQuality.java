9819234553fde6c1184b6b8ef8b947cd0d716da5
mihaylinsergey <semi999@mail.ru>
2022-04-07 23:03:44 +0300

    1. Динамическое перераспределение продуктов [#854]

--- src/main/java/ru/job4j/store/ControllQuality.java
+++ src/main/java/ru/job4j/store/ControllQuality.java
@@ -3,6 +3,7 @@ package ru.job4j.store;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class ControllQuality {
 
     private List<Store> list = new ArrayList<>();
@@ -22,4 +23,12 @@ public class ControllQuality {
         return new ArrayList<>(list);
     }
 
+    public void resort(List<Store> stores) {
+        List<Food> product = new ArrayList<>();
+         for (int i = 0; i < list.size(); i++) {
+             product.addAll(list.get(i).getList());
+        }
+        list.clear();
+        sort(stores, product);
+    }
 }
--- src/test/java/ru/job4j/store/ControllQualityTest.java
+++ src/test/java/ru/job4j/store/ControllQualityTest.java
@@ -47,4 +47,29 @@ public class ControllQualityTest {
                 LocalDate.now().minusDays(4),
                 84, 30))));
     }
+
+    @Test
+    public void resort() {
+        ControllQuality controllQuality = new ControllQuality();
+        Food sausage = new Meet("Sausage",
+                LocalDate.now().plusDays(100),
+                LocalDate.now().minusDays(20),
+                1000, 0);
+        Food bread = new Bread("Bread",
+                LocalDate.now().plusDays(9),
+                LocalDate.now().minusDays(4),
+                40, 0);
+        Food milk = new Milk("Milk",
+                LocalDate.now().minusDays(1),
+                LocalDate.now().minusDays(4),
+                60, 0);
+        List<Food> products = new ArrayList(List.of(sausage, bread, milk));
+        List<Store> stores = new ArrayList<>(List.of(new Warehouse(), new Shop(), new Trash()));
+        controllQuality.sort(stores, products);
+        controllQuality.resort(List.of(new Warehouse(), new Shop(), new Trash()));
+       assertThat(controllQuality.getStoreList().get(0).getList(), is(List.of(sausage)));
+       assertThat(controllQuality.getStoreList().get(1).getList(), is(List.of(bread)));
+       assertThat(controllQuality.getStoreList().get(2).getList(), is(List.of(milk)));
+    }
+
 }
